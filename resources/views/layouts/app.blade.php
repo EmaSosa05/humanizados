acd4682d395787233fb968dfa501dbbc53d01ace
Patrick <pmgesualdi@gmail.com>
2018-03-27 20:23:04 -0300

    Landing page updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,18 +4,22 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ asset('css/app.css')}} ">
-        <title>{{config('app.name', 'Humanizados')}}</title>
-
+        
+        <title>{{ config("app.name", "Humanizados") }}</title>
+        
+        <link rel="stylesheet" href="{{ asset("css/app.css") }}">
     </head>
     <body>
-        @include('inc.navbar')
-        <section class="section hero">
-            <div class="container">
-                @yield('content')
+        @include("inc.navbar")
+        <section class="hero">
+            <div class="content">
+                @yield("content")
             </div>
         </section>
+        <section id="entrevistas">
+            @yield("entrevistas")
+        </section>
 
-        <script src="{{ asset('js/app.js')}} "></script>
+        <script src="{{ asset("js/app.js") }}"></script>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,4 +3,30 @@
 @section('content')
     <h1>Bienvenidos al sitio de Humanizados</h1>
     <p>Entrevistas y diálogos íntimos con los personajes más reconocidos del ambiente.</p>
+    <a class="cta-entrevistas" href="#entrevistas">Entrevistas <i class="fa fa-camera"></i></a>
+@endsection
+
+@section('entrevistas')
+    <div class="columns">
+        <div class="column">
+            <iframe width="400" height="300" src="https://www.youtube.com/embed/v1Q_Cr6M1Ms" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="column">
+            <iframe width="400" height="300" src="https://www.youtube.com/embed/XKDGZ-VWLMg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="column">
+            <iframe width="400" height="300" src="https://www.youtube.com/embed/QYYWV7wqVps" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+    </div>
+    <div class="columns">
+        <div class="column">
+            <iframe width="400" height="300" src="https://www.youtube.com/embed/0ld5H-X8XIE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="column">
+            <iframe width="400" height="300" src="https://www.youtube.com/embed/dDL0joDF96U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div class="column">
+            <iframe width="400" height="300" src="https://www.youtube.com/embed/ps8D1Fu-K8E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+    </div>
 @endsection
